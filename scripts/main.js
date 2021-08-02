79ae50a6bda373ae6fc06160b976dbec3a619eaf
Thomas <thomasyao15@gmail.com>
2021-08-02 14:57:05 +1000

    Added object structure of books and library Added todo list

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,32 @@ function toggleModal() {
     const modal = document.querySelector(".modal-container");
     modal.classList.toggle("open");
 }
+
+
+function Library() {
+    this.books = [];
+    this.addBook = (title, author, pages, hasBeenRead) => {
+        const newBook = new Book(title, author, pages, hasBeenRead);
+        this.books.push(newBook);
+    };
+    this.deleteBook = (bookIndex) => {
+        this.books.splice(bookIndex, 1);
+    };
+}
+
+function Book(title, author, pages, hasBeenRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasBeenRead = hasBeenRead;
+}
+Book.prototype.toggleRead = () => {
+    this.hasBeenRead = !this.hasBeenRead;  // do you need this keyword?
+    console.log(`Has been read: ${this.hasBeenRead}`);
+};
+
+library = new Library();
+
+// TODO: add addBookHTML function that creates new book div with unique IDs
+// TODO: route modal inputs to addBook function
+// TODO: add deleteBook function - filters through following book divs and decrements IDs to match array index
